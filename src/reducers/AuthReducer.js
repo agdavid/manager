45fd9ba64c7ba581f73210360174ff603fdb4cf1
Ajay David <antonio.g.david@gmail.com>
2017-05-26 13:44:52 -0400

    update state in AuthReducer

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -5,7 +5,10 @@ const INITIAL_STATE = { email: '' };
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
-      
+      // To get react to see a new state and re-render components
+      // create a new state object with all the same key/values as original state
+      // then set/overwrite a specific key with the updated value
+      return { ...state, email: action.payload };
     default:
       return state;
   }
