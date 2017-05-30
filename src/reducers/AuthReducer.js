db38bfa0232be57d787e5a155a81dcd34cd01f62
Ajay David <antonio.g.david@gmail.com>
2017-05-30 10:44:59 -0400

    add LOGIN_USER_SUCCESS to action types and update reducer state

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,11 +1,13 @@
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
-  password: ''
+  password: '',
+  user: null
  };
 
 export default (state = INITIAL_STATE, action) => {
@@ -16,9 +18,11 @@ export default (state = INITIAL_STATE, action) => {
       // To get react to see a new state and re-render components
       // create a new state object with all the same key/values as original state
       // then set/overwrite a specific key with the updated value
-      return { ...state, email: action.payload };
+      return { ...state, email: action.payload }; //state.auth.email
     case PASSWORD_CHANGED:
-      return { ...state, password: action.payload };
+      return { ...state, password: action.payload }; //state.auth.password
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload }; //state.auth.user
     default:
       return state;
   }
