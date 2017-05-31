5e0caa12c5f03506954958862b422927d5f2e2fd
Ajay David <antonio.g.david@gmail.com>
2017-05-31 10:32:04 -0400

    add EmployeeFormReducer to reducers index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
+import EmployeeFormReducer from './EmployeeFormReducer';
 
 export default combineReducers({
   // set state to values returned by reducers
-  auth: AuthReducer
+  auth: AuthReducer,
+  employeeForm: EmployeeFormReducer
 });
