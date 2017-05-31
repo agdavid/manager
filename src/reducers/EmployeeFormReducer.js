806591bf3d5edf2318122916791de215251ed589
Ajay David <antonio.g.david@gmail.com>
2017-05-31 10:28:13 -0400

    add case to EmployeeFormReducer for EMPLOYEE_UPDATE

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,10 +1,20 @@
-import { EMPLOYEE_UPDATE } from './actions/types';
+import {
+  EMPLOYEE_UPDATE
+} from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
-  switch(action.type) {
+  switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      // action.payload = { prop: 'name', value: 'jane' }
+      // square braces are key interpolation to add { name: 'jane' } to state
+      return { ...state, [action.payload.prop]: action.payload.value }
     default:
       return state;
-  };
+  }
 };
