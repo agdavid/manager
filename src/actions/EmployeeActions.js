b8dc2c45ff2a8ec4b28c2f0d1a2fa62c1d203de9
Ajay David <antonio.g.david@gmail.com>
2017-06-01 09:38:36 -0400

    working push of data to firebase JSON database

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import { EMPLOYEE_UPDATE } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -8,5 +9,7 @@ export const employeeUpdate = ({ prop, value }) => {
 };
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  console.log(name, phone, shift);
+  const { currentUser } = firebase.auth(); //get current user
+  firebase.database().ref(`/users/${currentUser.uid}/employees`) //path through JSON data structure
+    .push({ name, phone, shift }); //save to referenced space
 };
