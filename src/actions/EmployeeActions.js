2b1d8be4e84d25b773460212cb6f632fd8d7e959
Ajay David <antonio.g.david@gmail.com>
2017-06-01 09:47:21 -0400

    route to employeeList scene after create

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { EMPLOYEE_UPDATE } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -10,6 +11,11 @@ export const employeeUpdate = ({ prop, value }) => {
 
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth(); //get current user
-  firebase.database().ref(`/users/${currentUser.uid}/employees`) //path through JSON data structure
-    .push({ name, phone, shift }); //save to referenced space
+  // use ReduxThunk to return function and 'fake' dispatch of action
+  return () => {
+     //path through JSON data structure
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift }) //save to referenced space
+      .then(() => Actions.employeeList({ type: 'reset' })); //route to scene, no back button
+  };
 };
