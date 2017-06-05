073aa8645096a853a214b1f88b0379f48bd6a5f8
Ajay David <antonio.g.david@gmail.com>
2017-06-05 08:41:51 -0400

    use EmployeeForm component in EmployeeCreate

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Button } from './common';
+import EmployeeForm from './EmployeeForm';
 
 class EmployeeCreate extends Component {
   onButtonPress() {
@@ -14,8 +15,7 @@ class EmployeeCreate extends Component {
   render() {
     return (
       <Card>
-
-
+        <EmployeeForm {...this.props}/>
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Create
--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, Picker } from 'react-native';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { CardSection, Input } from './common';
 
 class EmployeeForm extends Component {
@@ -52,4 +54,9 @@ const styles = {
   }
 };
 
-export default EmployeeForm;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeForm);
