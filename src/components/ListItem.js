ae10e11c78b795a78a5cc86165ee2fa148c2cc62
Ajay David <antonio.g.david@gmail.com>
2017-06-05 08:20:07 -0400

    add onPress to link to employeeCreate scene

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { CardSection } from './common';
 
 class ListItem extends Component {
+  onRowPress() {
+    Actions.employeeCreate({ employee: this.props.employee });
+  }
+
   render() {
     const { name } = this.props.employee;
 
     return (
-      <TouchableWithoutFeedback>
+      <TouchableWithoutFeedback
+        onPress={this.onRowPress.bind(this)}
+      >
         <View>
           <CardSection>
             <Text style={styles.titleStle}>
