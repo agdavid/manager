be3dd8ce045898dcfc94acefa4e5b8840ec60d8b
Ajay David <antonio.g.david@gmail.com>
2017-06-05 09:02:36 -0400

    use employeeUpdate action creator to populate state in EmployeeEdit

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,9 +1,22 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Button } from './common';
 
 class EmployeeEdit extends Component {
+  componentWillMount() {
+    //take all properties of this.props.employee
+    //forward to employeeUpdate action creator to hit reducer
+    //lodash to iterate over object
+    _.each(this.props.employee, (value, prop) => {
+      console.log(value);
+      console.log(prop);
+      this.props.employeeUpdate({ prop, value });
+    });
+  }
+
   render() {
     return (
       <Card>
@@ -18,4 +31,4 @@ class EmployeeEdit extends Component {
   }
 }
 
-export default connect()(EmployeeEdit);
+export default connect(null, { employeeUpdate })(EmployeeEdit);
