e2a94ec64b3188b3d6d2dfc6915a6e0ddbf82190
Ajay David <antonio.g.david@gmail.com>
2017-06-07 09:11:38 -0400

    add props to reusable Confirm component

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -3,18 +3,22 @@ import { Text, View, Modal } from 'react-native';
 import { CardSection } from './CardSection';
 import { Button } from './Button';
 
-const Confirm = () => {
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
   return (
     <Modal
+      visible={visible} // this.props.visible will be true/false
+      transparent
+      animationType="slide"
+      onRequestClose={() => {}} // android requires this prop, even if empty
     >
       <View>
         <CardSection>
-          <Text>
-          </Text>
+          <Text>{children}</Text>
         </CardSection>
+
         <CardSection>
-          <Button>Yes</Button>
-          <Button>No</Button>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
         </CardSection>
       </View>
     </Modal>
